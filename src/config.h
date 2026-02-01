e8c395c6ba69a7b7839c48784df0a9ca6326a47c
trace boggs <trace.boggs05@gmail.com>
2026-01-31 19:20:47 -0600

    Implemented Shader Class from LearnOpenGL

--- src/config.h
+++ src/config.h
@@ -2,3 +2,4 @@
 #include <iostream>
 #include <glad/glad.h> //must include glad before GLFW for compatability headache reasons
 #include <GLFW/glfw3.h>
+#include "shader.h" //custom shader class for easy handling of GLSL shaders
